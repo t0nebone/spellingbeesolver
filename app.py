13e6c37aab0b20132d05b6fe95d7c694d62090d9
Tony J <amj5072@gmail.com>
2020-03-29 16:50:16 -0400

    initial commit - before setup for heroku

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, escape
+from spellingb import search_for_words_web
+
+app = Flask(__name__)
+
+
+@app.route('/spellingbsolver', methods=['POST'])
+def do_search() -> str:
+    strip = "[]"
+    center_letter = request.form['center_letter']
+    outside_letters = request.form['outside_letters']
+    title = 'Spelling Bee Solved'
+    results = str(search_for_words_web(center_letter, outside_letters)).strip(strip).replace("'", "")
+
+    return render_template('results.html',
+                           the_center_letter=center_letter,
+                           the_outside_letters=outside_letters,
+                           the_title=title,
+                           the_results=results,
+                           )
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title="NYT Spelling Bee Solver")
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    contents = []
+    with open('spellingb.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[0].append(line.strip("[\"ImmutableMultiDict([('center_letter','")[0])
+                contents[1].append(
+                    line.strip("[\"ImmutableMultiDict([('center_letter', 'b'), ('outside_letters', '")[:5])
+                contents[-1].append(escape(item))
+
+    titles = ('Center Leter', 'Outside Letter', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents, )
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ spellingb.py
@@ -0,0 +1,48 @@
+from string import ascii_lowercase
+from nltk.corpus import words
+
+
+def search_for_words() -> list:
+    """Display all words that can be created with the asked for letters
+    following the rules of the NYTIMES spelling bee"""
+
+    all_words = words.words()
+    alphabet = tuple(ascii_lowercase)
+    req_letter = input('Enter the center letter ')
+    oth_letters = input('Enter the supporting letters')
+    all_letters = set(req_letter).union(set(oth_letters))
+    wrong_letters = set(alphabet).difference(all_letters)
+
+    # used a comprehension to eliminate the empty final_list[] variable
+    # but it does seem a bit complicated
+    final_list = [w
+                  for w in all_words
+                  if bool(wrong_letters.intersection(set(w))) is False and
+                  w.islower() is True and
+                  len(w) > 4 and
+                  req_letter in w
+                  ]
+    return final_list
+
+    # printing should be better but could probably manage better in webapp or gui
+    print(final_list)
+
+
+def search_for_words_web(center_letter: str, outside_letters: str) -> list:
+    """Display all words that can be created with the asked for letters
+    following the rules of the NYTIMES spelling bee"""
+    all_words = words.words()
+    alphabet = tuple(ascii_lowercase)
+    req_letter = center_letter
+    oth_letters = outside_letters
+    all_letters = set(req_letter).union(set(oth_letters))
+    wrong_letters = set(alphabet).difference(all_letters)
+
+    final_list = [w
+                  for w in all_words
+                  if bool(wrong_letters.intersection(set(w))) is False and
+                  w.islower() is True and
+                  len(w) > 4 and
+                  req_letter in w
+                  ]
+    return final_list
